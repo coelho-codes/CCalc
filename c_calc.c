e151ad4dc95d07c5f6490e98fce995f4d4b94a08
Davi Coelho <davicoelho.dev@gmail.com>
2024-05-14 15:41:41 -0300

    initial commit

--- c_calc.c
+++ c_calc.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
 
-int main()
+void intro();
+int calculate();
+
+void main()
 {
-    printf("Hello world!\n");
+    int result;
+
+    intro();
+    result = calculate();
+
+    printf("%d\n", result);
+}
+
+void intro()
+{
+    printf("Welcome to the C_Calculator!!!\n");
+    printf("There are 4 operations you can choose from: addition(+), subtraction(-), multiplication(*) and division(/)\n");
+}
+
+int calculate()
+{
+    int x, y, result;
+    char op;
+
+    printf("Please input the first number: ");
+    scanf("%d", &x);
+    printf("Please input the second number: ");
+    scanf("%d", &y);
+    printf("Please input the operation desired: ");
+    scanf(" %c", &op);
+
+    switch(op)
+    {
+        case '+':
+            result = x+y;
+            return result;
+            break;
+        case '-':
+            result = x-y;
+            return result;
+            break;
+        case '*':
+            result = x*y;
+            return result;
+            break;
+        case '/':
+            if(y == 0)
+            {
+                result = -1;
+                printf("ERROR: DIVISION BY ZERO\n");
+                return result;
+            }
+            else 
+            {
+                result = x/y;
+                return result;
+            } 
+        default:
+            printf("Operation not recognized\n");
+            return -1;
+    }
 }
